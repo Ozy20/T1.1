f9b06e5bc141de9c6640c607cd30c8ba3971400b
omar khaled <mer27320@gmail.com>
2023-07-14 01:19:59 +0300

    omar khaled

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.print("Programming is fun");
+    }
+}
